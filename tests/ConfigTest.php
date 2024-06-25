09b2804d69921adedf0c541731c296215590cec2
Kyrylo Silin <silin@kyrylo.org>
2024-06-25 21:07:13 +0300

    Add a way to configure the library

---
+++ tests/ConfigTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Telebugs\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use Telebugs\Config;
+
+class ConfigTest extends TestCase
+{
+  protected static $config;
+
+  protected function setUp(): void
+  {
+    self::$config = Config::getInstance();
+  }
+
+  protected function tearDown(): void
+  {
+    self::$config->reset();
+  }
+
+  public function testSetApiKey()
+  {
+    self::$config->setApiKey("123456");
+    $this->assertEquals("123456", self::$config->getApiKey());
+  }
+
+  public function testSetApiURL()
+  {
+    self::$config->setApiURL("https://api.telebugs.com/2024-03-28/errors");
+    $this->assertEquals("https://api.telebugs.com/2024-03-28/errors", self::$config->getApiURL());
+  }
+
+  public function testSetRootDirectory()
+  {
+    self::$config->setRootDirectory("/var/www/html");
+    $this->assertEquals("/var/www/html", self::$config->getRootDirectory());
+  }
+
+  public function testConfigure()
+  {
+    self::$config->configure([
+      'api_key' => "123456",
+      'api_url' => "https://api.telebugs.com/2024-03-28/errors",
+      'root_directory' => "/var/www/html"
+    ]);
+
+    $this->assertEquals("123456", self::$config->getApiKey());
+    $this->assertEquals("https://api.telebugs.com/2024-03-28/errors", self::$config->getApiURL());
+    $this->assertEquals("/var/www/html", self::$config->getRootDirectory());
+  }
+
+  public function testReset()
+  {
+    self::$config->reset();
+
+    $this->assertNull(self::$config->getApiKey());
+    $this->assertEquals("https://api.telebugs.com/2024-03-28/errors", self::$config->getApiURL());
+    $this->assertEquals("", self::$config->getRootDirectory());
+  }
+}
